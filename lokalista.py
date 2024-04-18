a2ca4fdb9fc6d7e1f34e358af8d0a5b096e33d3f
GreyedOutName <132237772+GreyedOutName@users.noreply.github.com>
2024-04-18 09:14:40 +0800

    Finalization of NotionAPI

--- lokalista.py
+++ lokalista.py
@@ -52,9 +52,9 @@ def main_interface():
     st.subheader("Total Price: " + str(total_price))
 
     st.header("Get your list in Notion!")
-    st.header("Remember to add connection to your Page")
+    st.header("Remember to add our integration to your Page")
     page_url=st.text_input(label="Enter your Notion Page ID here",value="https://www.notion.so/API-TEST-b7b7540389a84514a3ab6b49215817a9")
-    st.button("Save", key="btn2", on_click=notion,args=[page_url])
+    st.button("Save", key="btn2", on_click=notion,args=[page_url,df])
     
 def gsheet():   
     url = "https://docs.google.com/spreadsheets/d/1RiUc_unHWsdjHpAG8mIvMb1GD3_atoSSI_D_cSCCu2k/edit?usp=sharing"
@@ -62,7 +62,7 @@ def gsheet():
     data = conn.read(spreadsheet=url)
     return data
 
-def todolist(page_id,notion):
+def todolist(page_id,notion,content):
     notion.blocks.children.append(
         **{
             "block_id":page_id,
@@ -72,7 +72,7 @@ def todolist(page_id,notion):
                     {
                         "rich_text": [{
                             "text": {
-                                "content": notion,
+                                "content": content,
                             }
                         },
                         ],
@@ -84,7 +84,7 @@ def todolist(page_id,notion):
         }
     )
 
-def notion(page_url):
+def notion(page_url,df):
     page_id=(page_url[-32:])
     print(page_id)
    
@@ -109,37 +109,12 @@ def notion(page_url):
             ]
         }
     )
-    for x in range(5):
-        todolist(page_id,notion)
-
-    """
-    notion.blocks.children.append(
-        **{
-            "block_id":page_id,
-            "children":[
-                {
-                    "to_do": 
-                    {
-                        "rich_text": [{
-                            "text": {
-                                "content": "Finish Q3 goals",
-                            }
-                        },],
-                        "checked": False,
-                        "color": "default",
-                    }
-                }
-            ]
-        }
-    )
-    """
+    
+    for x in range(20):
+        todolist(page_id,notion,df[x].row.City)
 
 def generate():
     pass
 
-
-
-
-    
 main_interface()
 
